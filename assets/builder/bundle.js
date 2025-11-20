ba11df378a7da020b140a2eb2b80886f8a604271
carlosjimenohernandez <jimenohernandezcarlos@gmail.com>
2025-11-20 01:03:13 +0100

    borradas referencias

--- assets/builder/bundle.js
+++ assets/builder/bundle.js
@@ -2,6 +2,6 @@ require(`${__dirname}/vuebundler.js`).bundle({
   list: `${__dirname}/bundlelist.js`,
   output: `${__dirname}/../dist.js`,
   ignore: [],
-  id: "Traducciones_pdf_japones_application",
+  id: "Proyecto_base_001",
   module: false,
 });
