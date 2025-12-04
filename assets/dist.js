77fb8e628d3eda2e0255408271a5edb3fe140837
carlosjimenohernandez <jimenohernandezcarlos@gmail.com>
2025-12-05 00:36:53 +0100

    tester-viewer esconde barras de progreso completadas automatico

--- assets/dist.js
+++ assets/dist.js
@@ -19966,7 +19966,7 @@ Vue.component("NwtTesterNode", {
                 </div>
             </div>
         </div>
-        <div class="progress">
+        <div class="progress" v-if="node.progressBar.percent !== '100.00%'">
             <nwt-progress-bar-viewer :progress-bar="node.progressBar" />
         </div>
         <div class="children">
@@ -20572,6 +20572,136 @@ Vue.component("NwtSettingsViewer", {
  * 
  * 
  */
+
+NwtTester.global.define("Global test", async (test, assertion, subprogress) => {
+  test.define("First test", async (tester, assertion, subprogress) => {
+    subprogress.total = 8;
+    assertion(true, "assertion 1");
+    await NwtTimer.timeout(500);
+    subprogress.advance();
+    assertion(true, "assertion 2");
+    await NwtTimer.timeout(500);
+    subprogress.advance();
+    assertion(true, "assertion 3");
+    await NwtTimer.timeout(500);
+    subprogress.advance();
+    assertion(true, "assertion 4");
+    await NwtTimer.timeout(500);
+    subprogress.advance();
+    assertion(true, "assertion 5");
+    await NwtTimer.timeout(500);
+    subprogress.advance();
+    assertion(true, "assertion 6");
+    await NwtTimer.timeout(500);
+    subprogress.advance();
+    assertion(true, "assertion 7");
+    await NwtTimer.timeout(500);
+    subprogress.advance();
+    assertion(true, "assertion 8");
+    await NwtTimer.timeout(500);
+    subprogress.advance();
+  });
+  test.define("Second test", async (tester, assertion, subprogress) => {
+    subprogress.total = 3;
+    assertion(true, "assertion 1");
+    await NwtTimer.timeout(500);
+    subprogress.advance();
+    assertion(true, "assertion 2");
+    await NwtTimer.timeout(500);
+    subprogress.advance();
+    assertion(true, "assertion 3");
+    await NwtTimer.timeout(500);
+    subprogress.advance();
+  });
+  test.define("Third test", async (tester, assertion, subprogress) => {
+    subprogress.total = 5;
+    assertion(true, "assertion 1");
+    await NwtTimer.timeout(500);
+    subprogress.advance();
+    assertion(true, "assertion 2");
+    await NwtTimer.timeout(500);
+    subprogress.advance();
+    assertion(true, "assertion 3");
+    await NwtTimer.timeout(500);
+    subprogress.advance();
+    assertion(true, "assertion 4");
+    await NwtTimer.timeout(500);
+    subprogress.advance();
+    assertion(true, "assertion 5");
+    await NwtTimer.timeout(500);
+    subprogress.advance();
+  });
+  test.define("Fourth test", async (tester, assertion, subprogress) => {
+    tester.define("Fourth test - Part 1/5", async (tester, assertion, subprogress) => {
+      subprogress.total = 3;
+      assertion(true, "assertion 1");
+      await NwtTimer.timeout(500);
+      subprogress.advance();
+      assertion(true, "assertion 2");
+      await NwtTimer.timeout(500);
+      subprogress.advance();
+      assertion(true, "assertion 3");
+      await NwtTimer.timeout(500);
+      subprogress.advance();
+    });
+    tester.define("Fourth test - Part 2/5", async (tester, assertion, subprogress) => {
+      subprogress.total = 3;
+      assertion(true, "assertion 1");
+      await NwtTimer.timeout(500);
+      subprogress.advance();
+      assertion(true, "assertion 2");
+      await NwtTimer.timeout(500);
+      subprogress.advance();
+      assertion(true, "assertion 3");
+      await NwtTimer.timeout(500);
+      subprogress.advance();
+    });
+    tester.define("Fourth test - Part 3/5", async (tester, assertion, subprogress) => {
+      subprogress.total = 3;
+      assertion(true, "assertion 1");
+      await NwtTimer.timeout(500);
+      subprogress.advance();
+      assertion(true, "assertion 2");
+      await NwtTimer.timeout(500);
+      subprogress.advance();
+      assertion(true, "assertion 3");
+      await NwtTimer.timeout(500);
+      subprogress.advance();
+    });
+    tester.define("Fourth test - Part 4/5", async (tester, assertion, subprogress) => {
+      subprogress.total = 3;
+      assertion(true, "assertion 1");
+      await NwtTimer.timeout(500);
+      subprogress.advance();
+      assertion(true, "assertion 2");
+      await NwtTimer.timeout(500);
+      subprogress.advance();
+      assertion(true, "assertion 3");
+      await NwtTimer.timeout(500);
+      subprogress.advance();
+    });
+    tester.define("Fourth test - Part 5/5", async (tester, assertion, subprogress) => {
+      subprogress.total = 3;
+      assertion(true, "assertion 1");
+      await NwtTimer.timeout(500);
+      subprogress.advance();
+      assertion(true, "assertion 2");
+      await NwtTimer.timeout(500);
+      subprogress.advance();
+      assertion(true, "assertion 3");
+      await NwtTimer.timeout(500);
+      subprogress.advance();
+    });
+  });
+
+}, {
+  onTestDefined: t => trace(`TEST: ${t.name}`),
+  onTestSuccess: t => trace(`OK TEST: ${t.name}`),
+  onTestFailure: e => trace(`FAIL TEST: ${e.name} ${e.message}`),
+  onAssertionSuccess: (m) => trace(`OK ASSERTION ✔ ${m}`),
+  onAssertionFailure: (e) => trace(`FAIL ASSERTION ✘ ${e}`),
+});
+
 Vue.component("MainWindow", {
   template: `<div class="main_window_container">
     <div class="pad_1 pad_right_2">
@@ -20620,135 +20750,6 @@ Vue.component("MainWindow", {
   props: {},
   data() {
     trace("MainWindow.data");
-    NwtTester.global.progressBar.total = 3;
-    NwtTester.global.define("Global test", async (test, assertion, subprogress) => {
-      test.define("First test", async (tester, assertion, subprogress) => {
-        subprogress.total = 8;
-        assertion(true, "assertion 1");
-        await NwtTimer.timeout(500);
-        subprogress.advance();
-        assertion(true, "assertion 2");
-        await NwtTimer.timeout(500);
-        subprogress.advance();
-        assertion(true, "assertion 3");
-        await NwtTimer.timeout(500);
-        subprogress.advance();
-        assertion(true, "assertion 4");
-        await NwtTimer.timeout(500);
-        subprogress.advance();
-        assertion(true, "assertion 5");
-        await NwtTimer.timeout(500);
-        subprogress.advance();
-        assertion(true, "assertion 6");
-        await NwtTimer.timeout(500);
-        subprogress.advance();
-        assertion(true, "assertion 7");
-        await NwtTimer.timeout(500);
-        subprogress.advance();
-        assertion(true, "assertion 8");
-        await NwtTimer.timeout(500);
-        subprogress.advance();
-      });
-      test.define("Second test", async (tester, assertion, subprogress) => {
-        subprogress.total = 3;
-        assertion(true, "assertion 1");
-        await NwtTimer.timeout(500);
-        subprogress.advance();
-        assertion(true, "assertion 2");
-        await NwtTimer.timeout(500);
-        subprogress.advance();
-        assertion(true, "assertion 3");
-        await NwtTimer.timeout(500);
-        subprogress.advance();
-      });
-      test.define("Third test", async (tester, assertion, subprogress) => {
-        subprogress.total = 5;
-        assertion(true, "assertion 1");
-        await NwtTimer.timeout(500);
-        subprogress.advance();
-        assertion(true, "assertion 2");
-        await NwtTimer.timeout(500);
-        subprogress.advance();
-        assertion(true, "assertion 3");
-        await NwtTimer.timeout(500);
-        subprogress.advance();
-        assertion(true, "assertion 4");
-        await NwtTimer.timeout(500);
-        subprogress.advance();
-        assertion(true, "assertion 5");
-        await NwtTimer.timeout(500);
-        subprogress.advance();
-      });
-      test.define("Fourth test", async (tester, assertion, subprogress) => {
-        tester.define("Fourth test - Part 1/5", async (tester, assertion, subprogress) => {
-          subprogress.total = 3;
-          assertion(true, "assertion 1");
-          await NwtTimer.timeout(500);
-          subprogress.advance();
-          assertion(true, "assertion 2");
-          await NwtTimer.timeout(500);
-          subprogress.advance();
-          assertion(true, "assertion 3");
-          await NwtTimer.timeout(500);
-          subprogress.advance();
-        });
-        tester.define("Fourth test - Part 2/5", async (tester, assertion, subprogress) => {
-          subprogress.total = 3;
-          assertion(true, "assertion 1");
-          await NwtTimer.timeout(500);
-          subprogress.advance();
-          assertion(true, "assertion 2");
-          await NwtTimer.timeout(500);
-          subprogress.advance();
-          assertion(true, "assertion 3");
-          await NwtTimer.timeout(500);
-          subprogress.advance();
-        });
-        tester.define("Fourth test - Part 3/5", async (tester, assertion, subprogress) => {
-          subprogress.total = 3;
-          assertion(true, "assertion 1");
-          await NwtTimer.timeout(500);
-          subprogress.advance();
-          assertion(true, "assertion 2");
-          await NwtTimer.timeout(500);
-          subprogress.advance();
-          assertion(true, "assertion 3");
-          await NwtTimer.timeout(500);
-          subprogress.advance();
-        });
-        tester.define("Fourth test - Part 4/5", async (tester, assertion, subprogress) => {
-          subprogress.total = 3;
-          assertion(true, "assertion 1");
-          await NwtTimer.timeout(500);
-          subprogress.advance();
-          assertion(true, "assertion 2");
-          await NwtTimer.timeout(500);
-          subprogress.advance();
-          assertion(true, "assertion 3");
-          await NwtTimer.timeout(500);
-          subprogress.advance();
-        });
-        tester.define("Fourth test - Part 5/5", async (tester, assertion, subprogress) => {
-          subprogress.total = 3;
-          assertion(true, "assertion 1");
-          await NwtTimer.timeout(500);
-          subprogress.advance();
-          assertion(true, "assertion 2");
-          await NwtTimer.timeout(500);
-          subprogress.advance();
-          assertion(true, "assertion 3");
-          await NwtTimer.timeout(500);
-          subprogress.advance();
-        });
-      });
-
-    }, {
-      onTestDefined: t => trace(`TEST: ${t.name}`),
-      onTestSuccess: t => trace(`OK TEST: ${t.name}`),
-      onTestFailure: e => trace(`FAIL TEST: ${e.name} ${e.message}`),
-      onAssertionSuccess: (m) => trace(`OK ASSERTION ✔ ${m}`),
-      onAssertionFailure: (e) => trace(`FAIL ASSERTION ✘ ${e}`),
-    });
     return {
       toastCounter: 0,
       currentTester: NwtTester.global,
@@ -20816,8 +20817,10 @@ Vue.component("MainWindow", {
     trace("MainWindow.mounted");
     //this.$refs.btn1.click();
     this.currentTester.start();
-    if (window.AppRoot && window.AppRoot.initialize) {
-      window.AppRoot.initialize(this);
+    Inyeccion_a_modulo_externo: {
+      if (window.AppRoot && window.AppRoot.initialize) {
+        window.AppRoot.initialize(this);
+      }
     }
   }
 })
